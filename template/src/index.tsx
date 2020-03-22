730373adbcf7edc78168f6f3382bf057eee3937a
Sung M. Kim <sungmkim80@gmail.com>
2020-03-21 22:04:23 -0400

    For some reason tailwind folder didn't work after commit???

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind/index.css';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
