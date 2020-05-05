677ccec541bdd6d3b669defa554b2b6fbb4f482f
Ivo Angelov <jc@jagdcake.com>
2020-05-05 20:43:32 +0300

    Update Tailwind CSS to the latest version (1.4.4) (#9)
    * Update Tailwind CSS to the latest version
    
    Release notes for minor version:
    https://github.com/tailwindcss/tailwindcss/releases/tag/v1.4.0
    
    * Remove unnecessary packages and their configuration
    
    - @fullhuman/postcss-purgecss
    - purgecss
    
    * Configure Tailwind CSS option for purging unused CSS styles
    
    * Explain how Tailwind CSS handles its CSS file
    
    * Describe and comment out unused option

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,22 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './public/index.html',
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('postcss-preset-env'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
