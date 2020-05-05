5dfddd342a34951b79dfcbd3fea21ce1047acd4e
Ivo Angelov <jc@jagdcake.com>
2020-05-05 10:47:16 +0300

    Configure Tailwind CSS option for purging unused CSS styles

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: {
+    // Filenames to scan for classes
+    content: [
+      './src/**/*.html',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './public/index.html',
+    ],
+    // Options passed to PurgeCSS
+    options: {
+        whitelist: [],
+    },
+  },
 }
