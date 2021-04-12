1237587a9ca81d49940a274f79031818dc112fda
Sung M. Kim <sungmkim80@gmail.com>
2021-04-12 11:01:36 -0400

    feat: 🎸 "Jit" mode

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,18 +1,21 @@
 module.exports = {
+  // Uncomment the line below to enable the experimental Just-in-Time ("JIT") mode.
+  // https://tailwindcss.com/docs/just-in-time-mode
+  // mode: "jit",
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {},
   plugins: [],
   purge: {
     // Filenames to scan for classes
     content: [
-      './src/**/*.html',
-      './src/**/*.js',
-      './src/**/*.jsx',
-      './src/**/*.ts',
-      './src/**/*.tsx',
-      './public/index.html',
+      "./src/**/*.html",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.ts",
+      "./src/**/*.tsx",
+      "./public/index.html",
     ],
     // Options passed to PurgeCSS
     options: {
@@ -20,4 +23,4 @@ module.exports = {
       // safelist: [],
     },
   },
-}
+};
