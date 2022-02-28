7eda18184b320432f9c3cb0e2ec24d6fefc8f0fc
Arye Eidelman <aryedov.e@gmail.com>
2022-02-27 23:21:58 -0500

    Update tailwind config for v3 and consolidate content
    Based on https://tailwindcss.com/docs/upgrade-guide

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,26 +1,10 @@
 module.exports = {
-  // Uncomment the line below to enable the experimental Just-in-Time ("JIT") mode.
-  // https://tailwindcss.com/docs/just-in-time-mode
-  // mode: "jit",
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
-  purge: {
-    // Filenames to scan for classes
-    content: [
-      "./src/**/*.html",
-      "./src/**/*.js",
-      "./src/**/*.jsx",
-      "./src/**/*.ts",
-      "./src/**/*.tsx",
-      "./public/index.html",
-    ],
-    // Options passed to PurgeCSS
-    options: {
-      // Whitelist specific selectors by name
-      // safelist: [],
-    },
-  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./public/index.html",
+  ],
 };
