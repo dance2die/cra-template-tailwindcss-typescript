180de00e00fbb7d4fb3526c283d95e49b83daeea
Kolja P <34740585+ptaberg@users.noreply.github.com>
2022-05-28 17:52:48 +0300

    feat: remove extra imports of React (#25)

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-red-300 bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800">
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/tailwind.css";
 import App from "./App";
