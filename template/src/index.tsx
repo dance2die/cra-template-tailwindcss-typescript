4b3ba0ae4ec136e7a71eed0c4f750945a206fcd2
Samin Yaser <saminyaserwork@gmail.com>
2022-06-03 10:10:51 +0600

    replaced 'render' with 'createRoot'

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,9 +1,14 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client'
 import "./styles/tailwind.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
